b04c530a4cea015e7e97f6621dfc23c5444a0f31
D0nni387 <Richard.wells1987@hotmail.co.uk>
2020-06-27 15:00:40 +0100

    basked app initial commit with settings, urls add

--- FragileArt/settings.py
+++ FragileArt/settings.py
@@ -59,6 +59,7 @@ INSTALLED_APPS = [
     'store',
     'clients',
     'contact',
+    'basket',
 
     'storages',
 ]
--- FragileArt/urls.py
+++ FragileArt/urls.py
@@ -25,5 +25,6 @@ urlpatterns = [
     path('portfolio/', include('portfolio.urls')),
     path('store/', include('store.urls')),
     path('clients/', include('clients.urls')),
-    path('contact/', include('contact.urls'))
+    path('contact/', include('contact.urls')),
+    path('basket/', include('basket.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
