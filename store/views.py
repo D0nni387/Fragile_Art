7e2b0f94fb6843eb17dbc31fd672d118c945ec31
D0nni387 <Richard.wells1987@hotmail.co.uk>
2020-06-27 15:29:21 +0100

    added routes to view single item with links

--- store/views.py
+++ store/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 # Create your views here.
@@ -13,3 +13,15 @@ def store(request):
     }
 
     return render(request, 'store/store.html', context)
+
+    
+def item_detail(request, product_id):
+    """A View to display and individual product detail"""
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'store/item-detail.html', context)
