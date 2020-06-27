6742308d47a6407d5aab0040a7732a81e791936d
D0nni387 <Richard.wells1987@hotmail.co.uk>
2020-06-27 16:14:59 +0100

    added view to add item to basket with redirect

--- basket/views.py
+++ basket/views.py
@@ -1,11 +1,23 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
 def basket(request):
     """A View to return the users basket"""
     return render(request, 'basket/basket.html')
+
+def add_to_basket(request, item_id):
+    """ Add the requested item to the basket"""
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    basket = request.session.get('basket', {})
+
+
+    if item_id in list(basket.keys()):
+        basket[item_id] += quantity
+    else:
+        basket[item_id] = quantity
+
+    request.session['basket'] = basket
+    return redirect(redirect_url)
