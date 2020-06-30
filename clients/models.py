ee18773fa22f683575f16522b11e0ffcbf3087ad
D0nni387 <Richard.wells1987@hotmail.co.uk>
2020-06-30 17:36:36 +0100

    created models for clients app

--- clients/models.py
+++ clients/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Client(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.TextField(max_length=254)
+    business_type = models.CharField(max_length=50, null=True, blank=True)
+    description = models.TextField()
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
