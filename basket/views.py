4452bc268cb35c36bf2a7188fb4d9d66e7c0d8a8
D0nni387 <Richard.wells1987@hotmail.co.uk>
2020-07-04 18:00:52 +0100

    added views and url to remove items from the basket

--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, get_object_or_404, reverse
+from django.shortcuts import render, redirect, get_object_or_404, reverse, HttpResponse
 
 from store.models import Product
 
@@ -38,3 +38,17 @@ def update_basket(request, item_id):
 
     request.session['basket'] = basket
     return redirect(reverse('basket'))
+
+def delete_basket_item(request, item_id):
+    """ A view to delete selected item from the session basket """
+
+    item = get_object_or_404(Product, pk=item_id)
+    basket = request.session.get('basket', {})
+    basket.pop(item_id)
+
+    # Messages to be added later
+
+    request.session['basket'] = basket
+    return redirect(reverse('basket'), HttpResponse(status=200))
+
+    # error handling 
