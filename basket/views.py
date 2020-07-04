6db6b66d4044503f88aa19a7f29dec24e706bce7
D0nni387 <Richard.wells1987@hotmail.co.uk>
2020-07-04 17:13:50 +0100

    added view to update basket & url to suit

--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404, reverse
 
 from store.models import Product
 
@@ -16,11 +16,25 @@ def add_to_basket(request, item_id):
     redirect_url = request.POST.get('redirect_url')
     basket = request.session.get('basket', {})
 
-
     if item_id in list(basket.keys()):
         basket[item_id] += quantity
     else:
         basket[item_id] = quantity
+
+    request.session['basket'] = basket
+    return redirect(redirect_url)
+
+def update_basket(request, item_id):
+    """ A View to update the basket with """
+
+    item = get_object_or_404(Product, pk=item_id)
+    quantity = int(request.POST.get('quantity'))
+    basket = request.session.get('basket', {})
+
+    if quantity > 0:
+        basket[item_id] = quantity
+    else:
+        basket.pop[item_id]
 
     request.session['basket'] = basket
     return redirect(redirect_url)
