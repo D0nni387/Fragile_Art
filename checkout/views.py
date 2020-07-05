65263ae53a48e8d8c137bf58e4bc474d543b05c6
D0nni387 <Richard.wells1987@hotmail.co.uk>
2020-07-05 17:07:17 +0100

    updated basket views to get basket and forms

--- checkout/views.py
+++ checkout/views.py
@@ -1,9 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
 
+from .forms import OrderForm
 from store.models import Product
 
-# Create your views here.
 
 def checkout(request):
     """A View to return the checkout form"""
-    return render(request, 'checkout/checkout.html')
+    basket = request.session.get('basket', {})
+    if not basket:
+        messages.error(request, "Errrr there is nothing in here at all!")
+        return render(reverse('shop'))
+
+    order_form = orderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+    }
+
+    return render(request, template, context)
