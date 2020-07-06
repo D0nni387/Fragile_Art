72f389070eacef71c8a06f835d1d284d41be3092
D0nni387 <Richard.wells1987@hotmail.co.uk>
2020-07-06 15:37:42 +0100

    added stripe public and secret key references and views

--- FragileArt/settings.py
+++ FragileArt/settings.py
@@ -222,3 +222,9 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DELIVERY_PERCENTAGE = 15
+
+#stripe settings
+
+STRIPE_CURRENCY = 'GBP'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
--- checkout/views.py
+++ checkout/views.py
@@ -1,22 +1,43 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
 from store.models import Product
+from basket.contexts import basket_contents
+
+import stripe
 
 
 def checkout(request):
     """A View to return the checkout form"""
+
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     basket = request.session.get('basket', {})
     if not basket:
         messages.error(request, "Errrr there is nothing in here at all!")
         return redirect(reverse('store'))
 
+    current_basket = basket_contents(request)
+    total = current_basket['grand_total']
+    
+    #Stripe
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY
+    )
+
+    
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51GqJ5EHKfLOwRnJ3XFXbQ8xlzVlnViREL22CaqdLVUKpK4noXLjrl6HawbOUuIXQUZw3gg1Ib8pPuvIqQk4Pag1D00mheCMCdh'
+        'stripe_public_key': stripe_public_key, 
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
