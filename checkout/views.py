74cd925ba8e65ddf6da26b74879a25faf38abd22
D0nni387 <Richard.wells1987@hotmail.co.uk>
2020-07-06 19:23:47 +0100

    simplified views to remove redundancy

--- checkout/views.py
+++ checkout/views.py
@@ -21,51 +21,45 @@ def checkout(request):
 
         basket = request.session.get('basket', {})
 
-        form_input = {
+        form_data = {
             'first_name': request.POST['first_name'],
             'last_name': request.POST['last_name'],
             'email': request.POST['email'],
+            'telephone_number': request.POST['telephone_number'],
             'street_address1': request.POST['street_address1'],
             'street_address2': request.POST['street_address2'],
             'city_town': request.POST['city_town'],
-            'postcode_zip': request.POST['postcode_zip'],
             'county_state': request.POST['county_state'],
+            'postcode_zip': request.POST['postcode_zip'],
             'country': request.POST['country'],
         }
-        order_form = OrderForm(form_input)
+        order_form = OrderForm(form_data)
         if order_form.is_valid():
             order = order_form.save()
-            for item_id, item_data in basket.items():
+            for item_id, quantity in basket.items():
                 try:
                     product = Product.objects.get(id=item_id)
-                    if isinstance(item_data, int):
-                        order_line_item = OrderLineItem(
-                            order=order,
-                            product=product,
-                            quantity=item_data,
-                        )
-                        order_line_item.save()
-                    else:
-                        for size, quantity in item_data['items_by_size'].items():
-                            order_line_item = OrderLineItem(
-                                order=order,
-                                product=product,
-                                quantity=quantity,
-                                product_size=size,
-                            )
-                            order_line_item.save()
+                    order_line_item = OrderLineItem(
+                        order=order,
+                        product=product,
+                        quantity=quantity,
+                    )
+                    order_line_item.save()
+                    
                 except Product.DoesNotExist:
                     messages.error(request, (
                         "One of the products in your basket wasn't found. ")
                         
                     )
                     order.delete()
-                    return redirect(reverse('view_bag'))
+                    return redirect(reverse('view_basket'))
 
             request.session['save_info'] = 'save-info' in request.POST
+            
             return redirect(reverse('checkout_success', args=[order.order_number]))
         else:
             messages.error(request, 'There was an error with your submission, please recheck.')
+            return redirect(reverse('store'))
     else:
 
         basket = request.session.get('basket', {})
@@ -92,7 +86,6 @@ def checkout(request):
             'stripe_public_key': stripe_public_key, 
             'client_secret': intent.client_secret,
         }
-
         return render(request, template, context)
 
 def checkout_success(request, order_number):
@@ -105,10 +98,10 @@ def checkout_success(request, order_number):
     messages.success(request, f'Order place with number {order_number} \
         an e-mail has been sent to confirm!')
 
-    if basket in request.session:
+    if 'basket' in request.session:
         del request.session['basket']
 
-    template = 'checkout/checkout_success.html'
+    template = 'checkout/checkout-success.html'
     context = {
         'order': order,
     }
