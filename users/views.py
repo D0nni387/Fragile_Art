73ccc4566fbaaab2436afeebd0f90e6e76138527
D0nni387 <Richard.wells1987@hotmail.co.uk>
2020-07-20 18:47:27 +0100

    users profile app inital commit

--- users/views.py
+++ users/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def user(request):
+    """A View to display a users profile"""
+
+    context = {
+        
+    }
+
+    return render(request, 'users/user.html')
