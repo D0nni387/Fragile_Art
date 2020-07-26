3c17b52d6a49f82d4cb1c8d44dc1878c22c9c4a1
D0nni387 <Richard.wells1987@hotmail.co.uk>
2020-07-26 14:57:00 +0100

    added profile app with forms and basic layout

--- users/views.py
+++ users/views.py
@@ -1,13 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import UserProfile
+from .forms import ProfileForm
 
 
 # Create your views here.
 
 def user(request):
     """A View to display a users profile"""
+    user = get_object_or_404(UserProfile, user=request.user)
 
+    form = ProfileForm(instance=user)
+    orders = user.orders.all()
+
+    template = 'users/user.html'
     context = {
-        
+        'form': form,
+        'orders': orders,
     }
 
-    return render(request, 'users/user.html')
+    return render(request, template, context)
