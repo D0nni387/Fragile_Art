0b30b66d14d295d2a4a00f2e1f80895e52a44e46
D0nni387 <Richard.wells1987@hotmail.co.uk>
2020-10-18 21:20:01 +0100

    switched back to white noise temp

--- FragileArt/settings.py
+++ FragileArt/settings.py
@@ -81,6 +81,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'FragileArt.urls'
@@ -194,26 +196,26 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 
-if 'USE_AWS' in os.environ:
+# if 'USE_AWS' in os.environ:
 
-   AWS_S3_OBJECT_PARAMETERS = {
-       'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
-       'CacheControl': 'max-age=94608000',
-   }
+  #  AWS_S3_OBJECT_PARAMETERS = {
+  #      'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+  #      'CacheControl': 'max-age=94608000',
+  #  }
 
-   AWS_STORAGE_BUCKET_NAME = 'fragileart'
-   AWS_S3_REGION_NAME = 'eu-west-2'
-   AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-   AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-   AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+  #  AWS_STORAGE_BUCKET_NAME = 'fragileart'
+  #  AWS_S3_REGION_NAME = 'eu-west-2'
+  #  AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+  #  AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+  #  AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
-   STATICFILES_STORAGE = 'custom_storages.StaticStorage'
-   STATICFILES_LOCATION = 'static'
-   DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
-   MEDIAFILES_LOCATION = 'media'
+  #  STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+  #  STATICFILES_LOCATION = 'static'
+  #  DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+  #  MEDIAFILES_LOCATION = 'media'
 
-   STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
-   MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
+   # STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+   # MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
